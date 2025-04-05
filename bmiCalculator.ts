dde6c46654884c43acd29757af05ed834ade9a6a
Gilbert Marasigan <21050167+GilbertMarasigan@users.noreply.github.com>
2025-04-05 15:51:27 +0800

    9.3 command line bmiCalculator

--- bmiCalculator.ts
+++ bmiCalculator.ts
@@ -1,6 +1,25 @@
+interface bmiValues {
+    height: number;
+    weight: number;
+}
+
+const parseArguments = (args: string[]): bmiValues => {
+    if (args.length < 4) throw new Error('Not enough arguments');
+    if (args.length > 4) throw new Error('Too many arguments');
+  
+    if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+      return {
+        height: Number(args[2]),
+        weight: Number(args[3])
+      }
+    } else {
+      throw new Error('Provided values were not numbers!');
+    }
+  }
+
 const calculateBmi = (height: number, weight: number): string => {
     const bmi = weight / Math.pow((height / 100) , 2)
-    console.log('bmi', bmi)
+    
     let category;
     if(bmi < 18.5){
         category = 'Underweight'
@@ -14,8 +33,19 @@ const calculateBmi = (height: number, weight: number): string => {
     else{
         category = 'Obese'
     }
+
+    console.log(category)
     return `${category} range`
    
 }
 
-console.log(calculateBmi(180, 74))
+try {
+    const {height, weight } = parseArguments(process.argv);
+    calculateBmi(height, weight)
+} catch (error: unknown) {
+    let errorMessage = 'Something bad happened.'
+    if (error instanceof Error) {
+      errorMessage += ' Error: ' + error.message;
+    }
+    console.log(errorMessage);
+  }
