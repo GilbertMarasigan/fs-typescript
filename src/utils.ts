3ccf80f8235e964a1d21a68e329afe695fa848aa
Gilbert Marasigan <21050167+GilbertMarasigan@users.noreply.github.com>
2025-04-11 11:38:48 +0800

    use zod in utils vaidation and schema

--- src/utils.ts
+++ src/utils.ts
@@ -1,72 +1,16 @@
+import { z } from 'zod';
 import { NewPatientEntry, Gender } from "./types";
 
-const isString = (text: unknown): text is string => {
-    return typeof text === 'string' || text instanceof String;
-};
-
-const parseName = (name: unknown): string => {
-    if (!isString(name)) {
-        throw new Error('Incorrect comment');
-    }
-
-    return name;
-};
-
-const isGender = (param: string): param is Gender => {
-    return Object.values(Gender).map(v => v.toString()).includes(param);
-};
-
-const parseGender = (gender: unknown): Gender => {
-    if (!isString(gender) || !isGender(gender)) {
-        throw new Error('Incorrect gender: ' + gender);
-    }
-    return gender;
-};
-
-const parseOccupation = (occupation: unknown): string => {
-    if (!isString(occupation)) {
-        throw new Error('Incorrect occupation');
-    }
-    return occupation;
-};
-
-const isDateOfBirth = (date: string): boolean => {
-    return Boolean(Date.parse(date));
-};
-
-const parseDateOfBirth = (date: unknown): string => {
-    if (!date || !isString(date) || !isDateOfBirth(date)) {
-        throw new Error('Incorrect or missing date: ' + date);
-    }
-    return date;
-};
-
-const parseSSN = (ssn: unknown): string => {
-    if (!isString(ssn)) {
-        throw new Error('Incorrect ssn');
-    }
-
-    return ssn;
-};
+const newEntrySchema = z.object({
+    name: z.string(),
+    dateOfBirth: z.string().date(),
+    ssn: z.string().optional(),
+    gender: z.nativeEnum(Gender),
+    occupation: z.string()
+});
 
 export const toNewPatientEntry = (object: unknown): NewPatientEntry => {
-    if (!object || typeof object !== 'object') {
-        throw new Error('Incorrect or missing data');
-    }
-
-    if ('name' in object && 'dateOfBirth' in object && 'ssn' in object && 'gender' in object && 'occupation' in object) {
-        const newEntry: NewPatientEntry = {
-            name: parseName(object.name),
-            dateOfBirth: parseDateOfBirth(object.dateOfBirth),
-            ssn: parseSSN(object.ssn),
-            gender: parseGender(object.gender),
-            occupation: parseOccupation(object.occupation)
-        };
-
-        return newEntry;
-    }
-
-    throw new Error('Incorrect data: some fields are missing');
+    return newEntrySchema.parse(object);
 };
 
 export default toNewPatientEntry;
