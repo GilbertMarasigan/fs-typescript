4405c3f82ae421c000083e6dc9f017808b13dba9
Gilbert Marasigan <21050167+GilbertMarasigan@users.noreply.github.com>
2025-04-14 19:26:20 +0800

    9.24 patientor, step 4

--- src/components/PatientProfile.tsx
+++ src/components/PatientProfile.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import patientService from '../services/patients';
-import { Patient } from '../types';
+import { Patient, Entry, DiagnosisEntry } from '../types';
 
 const PatientProfile = () => {
     console.log('patient profile');
@@ -28,13 +28,26 @@ const PatientProfile = () => {
         return <div>Loading patient data...</div>;
     }
 
+    console.log('patient.entries', patient);
+
     return (
         <div>
-            <h2>{patient.name}</h2>
+            <h2>{patient.name} {patient.gender === 'male' ? ('♂') : ('♀')}</h2>
             <p>SSN: {patient.ssn}</p>
             <p>Occupation: {patient.occupation}</p>
-            <p>Gender: {patient.gender}</p>
-            <p>Date of Birth: {patient.dateOfBirth}</p>
+            <h3>entrieses</h3>
+            {
+                patient.entries.map((entry: Entry, index: number) => (
+                    <div key={index}>
+                        <div>{entry.date} <i>{entry.description}</i></div>
+                        <ul>
+                            {entry.diagnosisCodes?.map((code: string, index: number) => (
+                                <li key={index}>{code}</li>
+                            ))}
+                        </ul>
+                    </div>
+                )
+                )}
         </div>
     );
 };
--- src/types.ts
+++ src/types.ts
@@ -17,6 +17,61 @@ export interface Patient {
   gender: Gender;
   ssn?: string;
   dateOfBirth?: string;
+  entries: Entry[]
 }
+
+
+export interface DiagnosisEntry {
+  code: string
+  name: string,
+  latin?: string
+}
+
+
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: DiagnosisEntry['code'][]
+}
+
+interface SickLeave {
+  startDate: string;
+  endDate: string;
+}
+
+interface Discharge {
+  date: string;
+  criteria: string;
+}
+
+export enum healthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: healthCheckRating;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: SickLeave
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: Discharge
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
 
 export type PatientFormValues = Omit<Patient, "id" | "entries">;
