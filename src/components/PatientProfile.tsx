17fce3a6fc556c8a55f5a7e59620e60ee5acce46
Gilbert Marasigan <21050167+GilbertMarasigan@users.noreply.github.com>
2025-04-14 19:48:23 +0800

    9.25 patientor step 5

--- src/components/PatientProfile.tsx
+++ src/components/PatientProfile.tsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import patientService from '../services/patients';
-import { Patient, Entry, DiagnosisEntry } from '../types';
+import diagnosesService from '../services/diagnoses';
+import { Patient, Entry, Diagnosis } from '../types';
 
 const PatientProfile = () => {
     console.log('patient profile');
     const { id } = useParams();
     const [patient, setPatient] = useState<Patient | null>(null);
+    const [diagnoses, setDiagnoses] = useState<Diagnosis[]>([]);
 
     console.log('id', id);
 
@@ -21,12 +23,20 @@ const PatientProfile = () => {
                 console.error("Failed to tech patient", error);
             }
         };
+
+        const fetchDiagnosesList = async () => {
+            const diagnoses = await diagnosesService.getAll();
+            setDiagnoses(diagnoses);
+        };
         void fetchPatient();
+        void fetchDiagnosesList();
     }, [id]);
 
     if (!patient) {
         return <div>Loading patient data...</div>;
     }
+
+    console.log('diagnoses', diagnoses);
 
     console.log('patient.entries', patient);
 
@@ -35,14 +45,14 @@ const PatientProfile = () => {
             <h2>{patient.name} {patient.gender === 'male' ? ('♂') : ('♀')}</h2>
             <p>SSN: {patient.ssn}</p>
             <p>Occupation: {patient.occupation}</p>
-            <h3>entrieses</h3>
+            <h3>entries</h3>
             {
                 patient.entries.map((entry: Entry, index: number) => (
                     <div key={index}>
                         <div>{entry.date} <i>{entry.description}</i></div>
                         <ul>
                             {entry.diagnosisCodes?.map((code: string, index: number) => (
-                                <li key={index}>{code}</li>
+                                <li key={index}>{code} {diagnoses.find(d => d.code === code)?.name}</li>
                             ))}
                         </ul>
                     </div>
